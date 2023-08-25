ac17d90a3d6e0ff4ea9f3fd6820167c926c4ea7e
akash <gopanisuther17@gmail.com>
2023-08-25 08:48:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function addData() {
+    // console.log()
+    console.log(document.getElementById("firstname").value);
+    console.log(document.getElementById("lastName").value);
+    console.log(document.getElementById("email").value);
+    console.log(document.getElementById("password").value);
+    console.log(document.getElementById("cnic").value);
+    console.log(document.getElementById("mobileNumber").value);
+}
